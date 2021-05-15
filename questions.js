5e8565d7fa15252781bacea4982552b8bdc37544
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-15 18:16:05 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,23 @@
+//Array of objects
+const quiz = [
+    {
+        q:"The poet had written several ............... before completing the final version.",
+        options:["drafts", "originals", "poems", "novels", "rehearsals"],
+        answer: 0
+    },
+    {
+        q:"E-mails are normally ............... seconds after they have been sent.",
+        options:["copied", "delivered", "erased", "transformed", "inspected"],
+        answer: 1
+    },
+    {
+        q:"He fell on his knees, ............... for mercy.",
+        options:["respecting", "protesting", "begging", "resisting", "demanding"],
+        answer: 2
+    },
+    {
+        q:"Every working person must pay income ...............   .",
+        options:["tax", "salary", "wage", "price", "payment"],
+        answer: 0
+    }
+]
